81c648bb5601bcc6d5cd10c315e8a1802ac3940e
akaki99 <akakiqaradav12@gmail.com>
2024-10-20 20:23:24 +0400

    Lecture 13 Homework

---
+++ script.js
@@ -0,0 +1,9 @@
+const name = "აკაკი";
+const lastname = " ქარდავა ";
+let age = 22;
+const mail = " akakiqaradav12@gmail.com";
+const phone = 555548437;
+const adulthood = true;
+console.log(name + lastname + age + mail);
+console.warn(adulthood);
+console.error(phone);
